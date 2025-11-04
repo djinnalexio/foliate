djinnalexio/foliate

commit f1a1602a7bc57c4691d9435506d35d221b755da7

Update foliate-js
Metadata now contains localized strings.

See https://github.com/johnfactotum/foliate-js/commit/541bc07b6fd3eca9e1d63c3a80ce087955629d24

src/library.js
@@ -11,7 +11,7 @@ import { gettext as _ } from 'gettext'
 import * as utils from './utils.js'
 import * as format from './format.js'
 import { exportAnnotations } from './annotations.js'
-import { formatAuthors, makeBookInfoWindow } from './book-info.js'
+import { formatLanguageMap, formatAuthors, makeBookInfoWindow } from './book-info.js'
 
 import WebKit from 'gi://WebKit'
 import { WebView } from './webview.js'
@@ -246,7 +246,7 @@ const BookItem = GObject.registerClass({
     }
     update(item, data, cover) {
         this.#item = item
-        const title = data.metadata?.title ?? ''
+        const title = formatLanguageMap(data.metadata?.title)
         this._title.text = title
         this._image.load(cover?.then ? null : cover, title)
         this._progress.label = format.percent(fraction(data.progress))

src/book-info.js
@@ -5,10 +5,19 @@ import { gettext as _ } from 'gettext'
 import * as utils from './utils.js'
 import * as format from './format.js'
 
+export const formatLanguageMap = x => {
+    if (!x) return ''
+    if (typeof x === 'string') return x
+    const keys = Object.keys(x)
+    return /*x[format.matchLocales(keys)[0]] ??*/ x[keys[0]]
+}
+
 const formatContributors = contributors => Array.isArray(contributors)
     ? format.list(contributors.map(contributor =>
-        typeof contributor === 'string' ? contributor : contributor.name))
-    : contributors
+        typeof contributor === 'string' ? contributor
+        : formatLanguageMap(contributor?.name)))
+    : typeof contributors === 'string' ? contributors
+    : formatLanguageMap(contributors?.name)
 
 export const formatAuthors = metadata => metadata?.author
     ? formatContributors(metadata.author)
@@ -46,8 +55,7 @@ const makeSubjectBox = subject => {
         xalign: 0,
         wrap: true,
         selectable: true,
-        label: typeof subject === 'string'
-            ? subject : subject.name ?? subject.code,
+        label: formatContributors(subject) || subject?.code,
         valign: Gtk.Align.START,
     }), 'caption'))
     return box
@@ -64,14 +72,14 @@ const makeBookHeader = (metadata, pixbuf) => {
         xalign: 0,
         wrap: true,
         selectable: true,
-        label: metadata.title ?? '',
+        label: formatLanguageMap(metadata.title),
     }), 'title-2'))
 
     if (metadata.subtitle) box.append(utils.addClass(new Gtk.Label({
         xalign: 0,
         wrap: true,
         selectable: true,
-        label: metadata.subtitle,
+        label: formatLanguageMap(metadata.subtitle),
     }), 'title-4'))
 
     box.append(new Gtk.Label({
@@ -122,7 +130,7 @@ const makeBookInfoBox = (metadata, pixbuf) => {
     box.append(flowbox)
 
     for (const [title, value] of [
-        [_('Publisher'), metadata.publisher],
+        [_('Publisher'), formatContributors(metadata.publisher)],
         // Translators: this is the heading for the publication date
         [_('Published'), format.date(metadata.published)],
         // Translators: this is the heading for the modified date

src/book-viewer.js
@@ -18,7 +18,7 @@ import './navbar.js'
 import { AnnotationPopover, importAnnotations, exportAnnotations } from './annotations.js'
 import { SelectionPopover } from './selection-tools.js'
 import { ImageViewer } from './image-viewer.js'
-import { formatAuthors, makeBookInfoWindow } from './book-info.js'
+import { formatLanguageMap, formatAuthors, makeBookInfoWindow } from './book-info.js'
 import { themes, invertTheme, themeCssProvider } from './themes.js'
 import { dataStore } from './data.js'
 
@@ -725,10 +725,10 @@ export const BookViewer = GObject.registerClass({
         this._top_overlay_box.hide()
         this.#book = book
         book.metadata ??= {}
-        this.root.title = book.metadata.title ?? ''
-        this._book_title.label = book.metadata.title ?? ''
+        this._book_title.label = formatLanguageMap(book.metadata.title)
         this._book_author.label = formatAuthors(book.metadata)
-        this._book_author.visible = !!book.metadata?.author?.length
+        this._book_author.visible = !!this._book_author.label
+        this.root.title = this._book_title.label
 
         const { language: { direction } } = reader.view
         utils.setDirection(this._book_info, direction)

src/format.js
@@ -87,7 +87,7 @@ const getRegionEmoji = code => {
         ...Array.from(code.toUpperCase()).map(x => 127397 + x.charCodeAt()))
 }
 const displayName = new Intl.DisplayNames(locales, { type: 'language' })
-export const language = code => {
+const formatLangauge = code => {
     if (!code) return ''
     try {
         const locale = new Intl.Locale(code)
@@ -100,6 +100,11 @@ export const language = code => {
     } catch {
         return ''
     }
+}
+export const language = lang => {
+    if (typeof lang === 'string') return formatLangauge(lang)
+    if (Array.isArray(lang)) return list(lang.map(formatLangauge))
+    return ''
 }
 
 const minuteFormat = new Intl.NumberFormat(locales, { style: 'unit', unit: 'minute' })

src/reader/reader.js
@@ -22,6 +22,13 @@ const debounce = (f, wait, immediate) => {
     }
 }
 
+const formatLanguageMap = x => {
+    if (!x) return ''
+    if (typeof x === 'string') return x
+    const keys = Object.keys(x)
+    return x[keys[0]]
+}
+
 const getSelectionRange = sel => {
     if (!sel.rangeCount) return
     const range = sel.getRangeAt(0)
@@ -429,7 +436,7 @@ class Reader {
                 const { tocItem } = e.detail
                 heads.at(-1).innerText = tocItem?.label ?? ''
                 if (heads.length > 1)
-                    heads[0].innerText = this.book.metadata.title
+                    heads[0].innerText = formatLanguageMap(this.book.metadata.title)
             }
             if (feet) {
                 const { pageItem, location: { current, next, total } } = e.detail

commit ae8e573b198880ad7df7c5ad668acd0db284bad2

Fix crash and improve fractions in list view of library (#1602)
* library: correctly parse title in list view regardless of type

The title can be an object, a string or null. Setting a
label to null instead of "" is an uncatchable exception.
Thus, if this even happens, Foliate will crash.

Fixes: f1a1602a7bc57c4691d9435506d35d221b755da7 ("Update foliate-js")

Signed-off-by: Markus Göllnitz <camelcasenick@bewares.it>

* library: improve fraction calculation

With a zero-based index, this assumes if you are on a
page, that you read it partially exactly to the amount
of pages you read before relative to the amount of pages
before and after. That results in the first page being
considered unread and the last page being fully read of
you were on those pages.

While this is not accurate, there is no way to accurately
determine the current sub-page percentage a person read.
However, at least, this results in the percentage going
from 0 % to 100 % for any given book. Thus, opening a
book for the first time and not starting to read it marks
it as 0 %, which is the desired outcome of this change.

Signed-off-by: Markus Göllnitz <camelcasenick@bewares.it>

---------

Signed-off-by: Markus Göllnitz <camelcasenick@bewares.it>

src/library.js
@@ -219,7 +219,7 @@ GObject.registerClass({
     }
 })
 
-const fraction = p => p?.[1] ? (p[0] + 1) / (p[1] + 1) : null
+const fraction = p => !isNaN(p?.[1]) && p?.[1] > 0 ? p[0] / p[1] : null
 
 const BookItem = GObject.registerClass({
     GTypeName: 'FoliateBookItem',
@@ -279,7 +279,7 @@ const BookRow = GObject.registerClass({
     update(item, data) {
         this.#item = item
         const { metadata, progress } = data
-        const title = metadata?.title
+        const title = formatLanguageMap(metadata?.title)
         this._title.label = title
 
         const author = formatAuthors(metadata)